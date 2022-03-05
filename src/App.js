6fbeb25c2a90284c761a3351cbe8f57bc85692dc
Annika <85462906+AnniK93@users.noreply.github.com>
2022-03-05 22:06:28 +0000

    Rendered word meanings

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
+
         <main>
           <Dictionary />
         </main>
+
         <footer className="App-footer">
           <small>Coded by Anni</small>
         </footer>
