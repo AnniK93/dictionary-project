7fcabbd35864a35b6466b7c5ff86eb4819650af2
Annika <85462906+AnniK93@users.noreply.github.com>
2022-03-07 00:38:02 +0000

    Removed unnecessary code

--- src/Translation.js
+++ src/Translation.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-//import axios from "axios";
+import React from "react";
+
 import TranslationResults from "./TranslationResults";
 
 export default function Translation(props) {
