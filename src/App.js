227e856ac702694c6527b499e6adf46c2bb29ad1
Annika <85462906+AnniK93@users.noreply.github.com>
2022-03-09 21:09:00 +0000

    Removed logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import logo from "./logo.png";
+
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
+        <header className="App-header"></header>
 
         <main>
           <Dictionary />
